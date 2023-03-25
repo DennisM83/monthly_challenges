d445a487fef815450020ce6a0e55e51b7028f993
Dennis Wenrich <dennisfinley@gmail.com>
2023-03-25 11:29:55 -0400

    added a view for each month

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hey there!")
+
+def february(request):
+    return HttpResponse("February")
