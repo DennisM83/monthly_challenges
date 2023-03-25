5b01e848ec4076a2787432d16ca1fba5aef30c31
Dennis Wenrich <dennisfinley@gmail.com>
2023-03-25 12:15:54 -0400

    refactored for dynamic routing

--- challenges/views.py
+++ challenges/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
-def january(request):
-    return HttpResponse("Eat no meat for the entire month")
+# def january(request):
+#     return HttpResponse("Eat no meat for the entire month")
 
-def february(request):
-    return HttpResponse("Walk for at least 20 minutes everyday")
+# def february(request):
+#     return HttpResponse("Walk for at least 20 minutes everyday")
+
+def monthly_challenge(request, month):
+    if month == 'january':
+        challenge_text = "No meat for the enitre month"
+    elif month == 'february':
+        challenge_text = "Walk for at least 20 minutes a day"
+    elif month == 'march':
+        challenge_text = "No meat for the enitre month"
+    else:
+        return HttpResponseNotFound("Month not supported")
+    return HttpResponse(challenge_text)
